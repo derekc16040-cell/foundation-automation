3ee3f16afd5fd6937ff3c6c571895f2081f9c77e
Derek Christy <derekc16040@gmail.com>
2026-07-24 16:36:54 -0400

    Fix Clerk UserbUtton props

--- app/page.tsx
+++ app/page.tsx
@@ -258,7 +258,7 @@ export default async function Home() {
                 >
                   Account
                 </a>
-                <UserButton afterSignOutUrl="/" />
+                <UserButton />
               </>
             ) : (
               <a
